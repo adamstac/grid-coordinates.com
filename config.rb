ff547c8d3b4e82b82e8c60e880b99b153157c4f9
Adam Stacoviak <adam@stacoviak.com>
2011-01-11 12:14:12 -0600

    Updated Compass to output :expanded

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ css_dir = "themes/grid-coordinates/public/grid-coordinates/css"
 sass_dir = "themes/grid-coordinates/sass"
 images_dir = "themes/grid-coordinates/public/grid-coordinates/images"
 javascripts_dir = "js"
-output_style = :compressed #expanded #nested #compact
+output_style = :expanded #compressed #nested #compact
 environment = :production
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
