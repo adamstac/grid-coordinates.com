b698b232d20e980e683590da8518b4c9037468bf
Adam Stacoviak <adam@adamstacoviak.com>
2011-01-11 05:30:16 -0600

    Initial commit. Nesta theme and Compass setup

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'grid-coordinates'
+
+http_path = "/"
+css_dir = "themes/grid-coordinates/public/grid-coordinates/css"
+sass_dir = "themes/grid-coordinates/sass"
+images_dir = "themes/grid-coordinates/public/grid-coordinates/images"
+javascripts_dir = "js"
+output_style = :compressed #expanded #nested #compact
+environment = :production
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
