04c9f8e5126ddeeb3f535016fcaf988e37bb28f8
Adam Stacoviak <adam@adamstacoviak.com>
2011-01-12 01:18:02 -0600

    Add a /demo page

---
+++ themes/grid-coordinates/public/grid-coordinates/javascript/demo.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  // Create, show and hide the grid
+  $('body').append('<div id="grid"></div>');
+  $('body').append('<a href="#" class="toggle-grid"><span class="show">Show Grid</span><span class="hide">Hide Grid</span></a>');
+  $('a.toggle-grid').toggle(function() {
+    $('#grid').slideDown('fast');
+    $('.toggle-grid .hide').show();
+    $('.toggle-grid .show').hide();
+  }, function() {
+    $('#grid').slideUp('slow');
+    $('.toggle-grid .hide').hide();
+    $('.toggle-grid .show').show();
+  });
+  
+
+});
